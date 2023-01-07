83ebe29c1f2ea37aa82d1c1ea5cd169a6f98f188
John27200117 <122114472+John27200117@users.noreply.github.com>
2023-01-07 07:46:16 -0500

    Update practica2.py

--- practica2.py
+++ practica2.py
@@ -1,12 +1,15 @@
+#Funcion concertir un numero decimal a hexadecimal
 def convertirHexadecimal(numero):
+    #cadena de caracteres para representar los valores de la base
     conversionCadena = "0123456789ABCDEF"
-
+    #factor base
     if numero < 16:
         respuesta = conversionCadena[numero]
+    #factor recursivo
     else:
         respuesta = convertirHexadecimal(numero//16) + conversionCadena[numero%16]
     return respuesta
-
+#input para pedir el numero decimal a convertir
 numero = int(input("Ingrese el numero a convertir: "))
 resultado = convertirHexadecimal(numero)
 print(resultado)
