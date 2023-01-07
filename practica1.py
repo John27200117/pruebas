b17e051d457b6d7dd33499703b7b15ee5b424910
John27200117 <122114472+John27200117@users.noreply.github.com>
2023-01-07 07:42:13 -0500

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -1,7 +1,11 @@
+#Funcion sumatoria
 def K(n, p):
-    if n == 1:
+    #factor base
+    if n == 1: 
         return p
+    #factor recursivp
     return n*p + K(n-1, p)
+#los inputs para pedir que el usuarios ingresen los numeros
 n = int(input("Ingrese un numero n: "))
 p = int(input("Ingrese un numero p: "))
 print(K(n,p))
